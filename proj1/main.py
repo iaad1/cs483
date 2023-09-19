47fc6d01b7e1899c2835a296381f605de78acf63
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-09-19 16:08:46 -0700

    got it all working, simplifying output a little

--- proj1/main.py
+++ proj1/main.py
@@ -10,12 +10,12 @@ import math
 # Global vars
 CLASSES_LEN=7
 FEATURES=["hair","feathers","eggs","milk","airborne","aquatic","predator","toothed","backbone","breathes","venomous","fins","legs","tail","domestic","catsize"]
-ALPHA=1
+ALPHA=0.01
 # Functions
 
 def laPlaceIt(top, bottom):
-    numerator = top * ALPHA
-    denominator = bottom + (ALPHA * 2)
+    numerator = top + ALPHA
+    denominator = bottom + (ALPHA * CLASSES_LEN)
     return numerator / denominator
 
 # Returns a zero based array of the probabilites for each class 
@@ -28,30 +28,31 @@ def calcClassProbs(df):
     for i in range(CLASSES_LEN):
         classDf = df.query("class_type == " + str(i + 1)) # Get the rows of this class type (i + 1)
         num = len(classDf.index) # Count the rows.
-        arr.append(num / totalCnt) # Add the probability
+        arr.append(laPlaceIt(num, totalCnt)) # Add the probability
     return arr
 
 # Should return another zero based array of the class types, this time each one will contain a dict containing the conditional probability
 # IE: to get (feathers | mammal), one would do the following: arr[0]['feathers']
 def calcConditionalProbs(df):
-    totalCnt = len(df.index)
     arr = [] # Array of dicts
     # Loop over the classes again
     for i in range(CLASSES_LEN):
         featureDict = {}
+        className = str(i + 1)
+        classDf = df.query("class_type == " + className) # Get the rows of this class type
+        classCnt = len(classDf.index) # Count the rows.
         # Loop over the features
         for ft in FEATURES:
             # Generate a query string
-            qryStr = "class_type == " + str(i + 1) + " and "
-            # Do a check for legs, and do a different check
-            if ft == "legs":
-                qryStr += "legs > 0"
-            else:
-                qryStr += ft + " == 1"
-            rows = df.query(qryStr) # get all rows following the above
-            cnt = len(rows)
-            featureDict[ft] = laPlaceIt(cnt, totalCnt)
-            # Check if feature is 
+            qryStr = "class_type == " + className + " and "
+            # Get all unique values
+            vals = df[ft].unique()
+            for val in vals: # Loop through each unique value, and compute the prob for each
+                query = qryStr + ft + " == " + str(val)
+                key = ft + str(val) # Key for the dictionary
+                rows = df.query(query) # get all rows following the above
+                cnt = len(rows)
+                featureDict[key] = laPlaceIt(cnt, classCnt)
         arr.append(featureDict) # Add the dict to the array, pretty simple.
     return arr
 
@@ -59,17 +60,18 @@ def calcProbForRow(row, classProb, condProb: dict):
     cprob = 0 # Start at one, should be real small by the time we are done.
     # Loop through each feature, and multiply the ones we need to cprob
     for ft in FEATURES:
-        val = float(row.get(ft))
-        if (val > 0):
-            print(condProb[ft])
-            cprob +=  math.log2(condProb[ft])
-    return cprob
+        val = row.get(ft)
+        key = ft + str(val)
+        if key in condProb: # Possible we didn't have training data for this value, if not, skip.
+            cprob +=  math.log2(condProb[key]) # Underflow protection
+    cprob += math.log2(classProb) # add (multiply) by the probability of being the class
+    return math.pow(2, cprob) # Bring two to the power of cprob to get the actual prob
 
 def printCols(df):
-    cols = df.columns
-    cols = ",".join(cols)
+    cols = list(df.columns)
     # Add the three additional columns
-    cols += ",predicted,probability,correct?"
+    cols.extend(["predicted", "probability", "correct?"])
+    cols = ",".join(cols)
     print(cols)
 
 # Begin the program
@@ -100,11 +102,11 @@ for i, row in test.iterrows():
         condProb = condProbs[j]
         prob = calcProbForRow(row, classProb, condProb)
         probs.append(prob)
-    print(probs)
     # Get the largest value in the list
     biggest = max(probs)
     # Get the class number that it is (0 indexed)
     cIndex = probs.index(biggest)
+
     # Turn the class into a string (1 indexed)
     classGuess = str(cIndex + 1)
     # Get the sum of all the probs (for normalizing)
@@ -112,13 +114,12 @@ for i, row in test.iterrows():
     normalized = biggest / psum
     # Get the rows data
     rowArr = row.to_list()
-    # Turn to csv string
-    rowStr = ",".join(map(str, rowArr))
 
     # Get the ACTUAL class type
-    actualClass = row.get('class_type')
+    actualClass = str(row.get('class_type'))
     correct = "CORRECT" if actualClass == classGuess else "wrong"
-
-    # Add the three new values
-    rowStr += classGuess + "," + str(normalized) + "," + correct
+    # Add the three values to the row
+    rowArr.extend([classGuess, str(normalized), correct])
+    # Turn to csv string
+    rowStr = ",".join(map(str, rowArr))
     print(rowStr)
