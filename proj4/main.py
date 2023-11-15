fc7186a79f7720b29cd7b660fd039fef23fb430e
j.tanninen3 <j.tanninen3@students.clark.edu>
2023-11-15 12:36:28 -0800

    added comments, formatting

--- proj4/main.py
+++ proj4/main.py
@@ -14,9 +14,6 @@ from sklearn.metrics import silhouette_score
 import warnings
 warnings.filterwarnings('ignore')
 
-# Helper functions
-
-
 
 # Read the csv
 df = pd.read_csv('Pokemon.csv')
@@ -39,22 +36,25 @@ for type in types:
     typeDf = df.query("`Type 1` == '" + type + "'")
     # Limit the dataframe to just numerical columns
     typeDf = typeDf[number_cols]
-    n_samples = len(typeDf.index) # Number of rows
-    # Create a pipeline
+    n_samples = len(typeDf.index) # Number of rows for this type, need this to cap the number of clusters we can use
+
+    # Loop for each number of clusters.
     for n_clusters in cluster_range:
-        if (n_clusters >= n_samples): continue # Skip if not enough data.
+        if (n_clusters >= n_samples): continue # Skip if not enough data. (need more samples then clusters)
         pipe = Pipeline([
             ('scaler', MinMaxScaler()),
             ('kmeans', KMeans(n_clusters=n_clusters, n_init="auto"))
         ])
         # Fit the data
         pipe.fit(typeDf)
-        # Get just the kmeans part of the pipe, so we can read data from it
+        # Get just the kmeans part of the pipe, so we can get the labels from it.
         kmean = pipe[1]
         labels = kmean.labels_
-        # Check if number of labels is greater then n_samples - 1
+
+        # Calculate the silhouette score for the data, this way we can see how good the number of clusters was
         score = silhouette_score(typeDf, labels)
-        if (type not in stored_data or stored_data[type]["score"] < score):
+        if (type not in stored_data or stored_data[type]["score"] < score): # Check if this score was better than previous, if so, update the stored data with it
+            # Also, if we have no data, store it.
             stored_data[type] = {
                 "labels": labels,
                 "score": score,
@@ -70,6 +70,7 @@ for type in types:
 
 # Now, we have the results of all the kmeans and scores.
 
+# Create a new array for the columns we want to print out.
 printCols = ["Name"] + number_cols
 # Loop through the types again
 for type in types:
@@ -82,10 +83,12 @@ for type in types:
     for n_cluster in range(0, typeData["n_clusters"]):
         # Filter data for the current cluster
         cluster_data = typeDf[typeData["labels"] == n_cluster]
-        # Get the averages for all the stats
+        # Get the averages for all the stats in this cluster.
         averages = np.mean(cluster_data[number_cols], axis=0)
+
+        # All calculation now done, begin printing the last things.
+
         print("Cluster " + str(n_cluster))
-        # Print just the print columns
         print(cluster_data[printCols])
 
         # Loop through the number columns and print all those averages
